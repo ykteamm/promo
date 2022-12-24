ce391f727506e39d8583497fccc0027978507991
Ergashev Tolqin <ergashevtolqin0103@gmail.com>
2022-12-24 17:43:24 +0500

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,5 +20,46 @@
 <div class="container text-center text-white" style="margin-top: 20%;">
     <h3 class="mb-1" style="font-family: Supercell-Magic">{{Auth::user()->last_name}} {{Auth::user()->first_name}}</h1>
     <h1 style="font-family: Supercell-Magic">{{ Auth::user()->pharmacy }}</h1>
+    <div id="map" style="height: 300px"></div>  
+
 </div>
+@endsection
+@section('scripts')
+<script>
+    ymaps.ready(init);
+
+function init() {
+    var geolocation = ymaps.geolocation,
+        myMap = new ymaps.Map('map', {
+            center: [55, 34],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        });
+
+    // Сравним положение, вычисленное по ip пользователя и
+    // положение, вычисленное средствами браузера.
+    geolocation.get({
+        provider: 'yandex',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Красным цветом пометим положение, вычисленное через ip.
+        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        result.geoObjects.get(0).properties.set({
+            balloonContentBody: 'Мое местоположение'
+        });
+        myMap.geoObjects.add(result.geoObjects);
+    });
+
+    geolocation.get({
+        provider: 'browser',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Синим цветом пометим положение, полученное через браузер.
+        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+        result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+        myMap.geoObjects.add(result.geoObjects);
+    });
+}
+</script>
 @endsection
--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -16,3 +16,4 @@
 
 <!-- page level custom script -->
 <script src="{{asset('mobile/js/app.js')}}"></script>
+<script src="https://api-maps.yandex.ru/2.1/?apikey=7a4a276f-4b19-448d-877b-1c87a0b350c3&lang=ru_RU" type="text/javascript"></script>
